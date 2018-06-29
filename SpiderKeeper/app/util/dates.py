9274adc19ce52e70c0513c28a29de2e158440d23
huochen <huichen90>
2018-06-29 16:33:41 +0800

    添加视频列表和视频详情

--- SpiderKeeper/app/util/dates.py
+++ SpiderKeeper/app/util/dates.py
@@ -2,16 +2,22 @@ import time
 
 
 def dts2ts(datestr):
-    '''
+    """
     datestring translate to timestamp
-    '''
+    :param datestr:
+    :return: timeStamp
+    """
+
     timeArray = time.strptime(datestr, "%Y-%m-%d")
     timeStamp = int(time.mktime(timeArray))
     return timeStamp
 
 def ts2dts(timeStamp):
-
-    '''timestamp translate to datestring'''
+    """
+    timestamp translate to datestring
+    :param timeStamp:
+    :return: datestr
+    """
     timeArray = time.localtime(timeStamp)
     datestr = time.strftime("%Y-%m-%d", timeArray)
     return datestr
